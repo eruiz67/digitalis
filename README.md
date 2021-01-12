46fb83abe57cd53c22fbbc7d6f2aad58678c8a57
Ernesto Ruiz <ernesto930526@gmail.com>
2021-01-12 08:58:48 -0500

    Ajustes finales

--- README.md
+++ README.md
@@ -1,11 +1,6 @@
 # digitalis
-Desarrolle un módulo que agregue un nuevo tab en la aplicación de Proyecto que permita:
-- Agregar clientes
-- De cada cliente se desea tener el estado del mismo
-- Los estados posibles tienen que poder ser gestionados desde la aplicación de Proyecto
-- La edición de los estados de los clientes en el proyecto solo la puede hacer un usuario con permisos de administración
-- Los posibles estados de un cliente en un proyecto son propios de cada compañía (duda)
 
 ACLARACIONES
 - Para crear un estado es necesario tener el permiso de administracion del modulo de proyecto
-- Se ha implementado filtro de busqueda para el estado
+- Primeramente se crean los estados para una compañia, luego al adicionar un cliente a un proyecto solamente se podrán seleccionar los estados posibles de la compañia a la que pertenece el cliente. En el caso de que el cliente sea una compañia se seleccionarán los estados definidos para ella misma. 
+- Desde la vista kanban se pueden acceder a los clientes de un proyectos y adicionar nuevos. Tambien al dar click en el boton editar se puede ver una pestaña con los clientes.
--- project_digitalis/models/models.py
+++ project_digitalis/models/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class EstadoCliente(models.Model):
     _name = 'project_digitalis.state'
@@ -9,19 +10,73 @@ class EstadoCliente(models.Model):
     name = fields.Char(string='Estado', required=True)
     description = fields.Char(string='Descripcion')
 
+    partner_id = fields.Many2one('res.partner', string='Compañia', required=True, domain="[('is_company','=',True)]")
+
     _sql_constraints = [
         ('name_unique',
          'UNIQUE(name)',
          "Ya existe un estado con el mismo nombre")    ]
 
-class StatePartner(models.Model):
-    _inherit = "res.partner"
-    _description = 'Agrega Atributo Conduce Viveros'
+    
+class ProjectPartners(models.Model):
+    _inherit ="project.project"
+    _description = 'Permite adicionar clientes a los proyectos'
 
-    state = fields.Many2one('project_digitalis.state', string='Estado')
+    partner_state_ids = fields.One2many('project_digitalis.partner_state', 'project_id', string='Clientes') 
 
     
+    @api.constrains('partner_state_ids')
+    def _constrains_partner_state_ids(self):        
+        for i in range(0, len(self.partner_state_ids)-1):
+            for j in range(i+1,len(self.partner_state_ids)):
+                if self.partner_state_ids[i].partner_id == self.partner_state_ids[j].partner_id:
+                    raise ValidationError("El cliente '{}' se ha adicionado más de una vez. Elimine los clientes repetidos para el mismo proyecto".format(self.partner_state_ids[j].partner_id.name)) 
 
+
+    partner_count = fields.Integer(compute='_compute_partner_count', string='Cantidad Clientes')
+    label_partner_count = fields.Char(string='Para clientes', compute='_compute_partner_count', default='Cliente(s)')
+    
+    
+    @api.depends('partner_state_ids')
+    def _compute_partner_count(self):
+        for rec in self:
+            rec.partner_count = len(rec.partner_state_ids)
+            if len(rec.partner_state_ids) == 1:
+                rec.label_partner_count="Cliente"
+            else:
+                rec.label_partner_count="Clientes"
+
+    def action_view_partners(self):
+        action = self.with_context(active_id=self.id, active_ids=self.ids) \
+            .env.ref('project_digitalis.act_project_partner_tree_view') \
+            .sudo().read()[0]
+        action['display_name'] = self.name
+        return action
+
+class PartnerSate(models.Model):
+    _name = 'project_digitalis.partner_state'
+    _description = 'Permite adicionar un cliente a u proyecto con un estado determinado'
+
+    partner_id = fields.Many2one('res.partner', string='Cliente', required=True)
+    state_id = fields.Many2one('project_digitalis.state', string='Estado', required=True, help="Solamente se pueden poner los estados definidos para la compañia del cliente")
+    project_id = fields.Many2one('project.project', string='Proyecto')
+
+    def name_get(self):
+        result = []
+        for rec in self:
+            name = rec.project_id.name +" - "+ rec.partner_id.name
+            result.append((rec.id, name))
+        return result
+
+    @api.onchange('partner_id')
+    def _onchange_partner_id(self):
+        self.state_id = False
+        if self.partner_id.is_company:
+            return {'domain':{'state_id':[('partner_id','=',self.partner_id.id)]}}
+        else:
+            return {'domain':{'state_id':[('partner_id','=',self.partner_id.parent_id.id)]}}
+
+    
 # class project_digitalis(models.Model):
 #     _name = 'project_digitalis.project_digitalis'
 #     _description = 'project_digitalis.project_digitalis'
